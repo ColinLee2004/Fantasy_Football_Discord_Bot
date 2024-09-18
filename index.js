55c656f257b8e39731493d55592109d0333bfa08
ColinLee2004 <colinaslanlee@gmail.com>
2024-09-18 09:35:13 -0400

    Connected w/ OpenAi API

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 // Require the necessary discord.js classes
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 require('dotenv').config();
+const OpenAI = require("openai");
 
 const prefix = '.';
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages] });
+
+// Create an OpenAi client instance
+
+const openaiClient = new OpenAI({
+    apiKey: process.env.OPEN_AI_API_KEY
+})
 
 // When the client is ready, run this code (only once).
 // The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
@@ -14,7 +21,7 @@ client.once(Events.ClientReady, readyClient => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
-client.on(Events.MessageCreate, message => {
+client.on(Events.MessageCreate, async message => {
     //if (!message.content.startsWith(prefix)) return;
     let args = message.content.slice(prefix.length).split(' ');
     let command = args.shift().toLowerCase();
@@ -23,8 +30,35 @@ client.on(Events.MessageCreate, message => {
 
 
     }
+    let prompt = 'You are a bot that will take a screen shot of a fantasy football app ' + 
+    'and output a JSON dictionary where the keys are the players\' names and the values are arrays, '+
+    'where the first element is the player\'s projected points and the second element is the actualy '+
+    'amount of points that player scored.';
 
-    console.log(message);
+    if (message.author.bot) {
+        return; 
+   }
+    const completion = await openaiClient.chat.completions.create({
+    model : "gpt-4o",
+    messages : [
+        {
+            role : "system",
+            content : 'hi'
+        },
+        {
+            role : "user",
+            content : message.content
+        }
+    ]
+    })
+
+    let imageUrlArray = [];
+    for (const [snowflake, attachment] of message.attachments) {
+        imageUrlArray.push(attachment.url);
+    }
+
+    message.reply(completion.choices[0].message.content);
+    console.log(imageUrlArray);
 });
 
 
